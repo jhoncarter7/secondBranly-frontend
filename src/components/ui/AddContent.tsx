cb88d619982a8f1cd68c57f41ff85d8819a9303a
nikhilQuadb <rainikhil044@gmail.com>
2025-01-27 01:31:23 +0530

    refactor AddContent and Dashboard components to manage modal state, fetch content from API, and improve UI layout

--- src/components/ui/AddContent.tsx
+++ src/components/ui/AddContent.tsx
@@ -1,9 +1,9 @@
-import React, { useRef } from 'react'
+import React, { HtmlHTMLAttributes, useRef } from 'react'
 import InputBox from './InputBox'
 import Button from './Button'
 import axios from 'axios'
 
-const AddContent = () => {
+const AddContent = ({onClose}: {onClose : ()=> void}) => {
 const typeRef = useRef<HTMLInputElement>()
 const titleRef = useRef<HTMLInputElement>()
 const linkRef = useRef<HTMLInputElement>()
@@ -18,13 +18,17 @@ const linkRef = useRef<HTMLInputElement>()
     title,
     link
    })
+   onClose()
   }
   return (
-    <div className=' h-screen fixed top-0 left-0  bg-slate-400 opacity-80 w-full '>
+    <div className=' h-screen fixed top-0 left-0  bg-slate-400 opacity-60 w-full '>
         <div className='flex justify-center items-center h-full '>
      
         <form action="" onSubmit={addContentHandler} className='w-72 border rounded-lg bg-slate-100 flex flex-col justify-center items-center gap-y-3 py-4 '>
-        <h1>Add content</h1>
+        <div className='flex w-full '>
+        <h1 className='mx-auto'>Add content</h1>
+        <div className='flex  justify-self-end pr-3 cursor-pointer' onClick={onClose}>X</div>
+        </div>
         <InputBox type="string" placeholder='Type' reference={typeRef}/>
         <InputBox type="string" placeholder='Title' reference={titleRef} />
         <InputBox type="string" placeholder='link' reference={linkRef}/>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import SideBar from '../components/ui/SideBar'
 import Button from '../components/ui/Button'
 import Card from '../components/ui/Card'
@@ -6,10 +6,21 @@ import ShareIcon from '../icons/ShareIcon'
 import PlusIcon from '../icons/PlusIcon'
 import { useNavigate } from 'react-router-dom'
 import Model from '../components/ui/AddContent'
+import axios from 'axios'
 
 const Dashboard = () => {
+  const [modelOpen, setModelOpen] = useState(false)
+  const [getAllcontent, setGetAllcontent] = useState([])
   const navigate = useNavigate()
-   
+
+  useEffect(() => {
+    const fetchData = async () => {
+   const response =  await axios.get('/api/v1/content/getcontent')
+   setGetAllcontent(response.data.content)
+    }
+    fetchData()
+  }, [])
+console.log("getAllcontent", getAllcontent)
   return (
     <div className="flex  h-[90rem] w-full relative">
       <SideBar />
@@ -17,17 +28,21 @@ const Dashboard = () => {
         <div className='flex justify-between '>
           <h1 className='text-3xl font-semibold'>All Notes</h1>
           <div className='flex gap-x-2'>
-          <Button variant="secondary" size="md" text="Share Brain" startIcon={<ShareIcon size='lg'/>} onclick={() =>navigate('/login')}/>
-          <Button variant="primary" size="lg" text="Add Content" startIcon={<PlusIcon size='lg'/>}/>
+            <Button variant="secondary" size="md" text="Share Brain" startIcon={<ShareIcon size='lg' />}  />
+            <Button variant="primary" size="lg" text="Add Content" startIcon={<PlusIcon size='lg' />} onclick={()=> setModelOpen(true)}/>
           </div>
         </div>
-         
+
         <div className='w-full flex flex-wrap gap-x-2 pt-12'>
-        <Card title="twitter"  link='https://x.com/Shehzad_Ind/status/1882756224762188092' type='twitter'/>
-        {/* <Card/> */}
+         { getAllcontent.map(({title, type, link})=> (
+          <Card  type={type} title={title} link={link} />
+         ))  
+         
+         }
+          {/* <Card/> */}
         </div>
       </div>
-  <Model/>
+      {modelOpen && <Model onClose={() => setModelOpen(false)} />}
     </div>
   )
 }
