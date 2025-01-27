59e43c2633875be53a9abb5ea8460f41a6630b20
nikhilQuadb <rainikhil044@gmail.com>
2025-01-27 21:42:22 +0530

    add shareContent function to Dashboard for generating shareable links and update useEffect dependency

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -11,16 +11,36 @@ import axios from 'axios'
 const Dashboard = () => {
   const [modelOpen, setModelOpen] = useState(false)
   const [getAllcontent, setGetAllcontent] = useState([])
+  const [generatedLink, setGeneratedLink] = useState()
   const navigate = useNavigate()
+
+
 
   useEffect(() => {
     const fetchData = async () => {
-   const response =  await axios.get('/api/v1/content/getcontent')
-   setGetAllcontent(response.data.content)
+      const response = await axios.get('/api/v1/content/getcontent')
+      setGetAllcontent(response.data.content)
     }
     fetchData()
-  }, [])
-console.log("getAllcontent", getAllcontent)
+  }, [modelOpen])
+
+
+  const shareContent = async () => {
+    try {
+      await axios.post('/api/v1/link/share', {
+        share: true
+      }).then((resp)=> setGeneratedLink(resp.data.hash))
+      // if (response.data?.success == true) {
+      //   await axios.post(`/api/v1/link/${response.data.hash}`).then((resp) => setGeneratedLink(resp.data))
+      // }
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+
+
+  console.log("generatedLink", generatedLink)
   return (
     <div className="flex  h-[90rem] w-full relative">
       <SideBar />
@@ -28,17 +48,17 @@ console.log("getAllcontent", getAllcontent)
         <div className='flex justify-between '>
           <h1 className='text-3xl font-semibold'>All Notes</h1>
           <div className='flex gap-x-2'>
-            <Button variant="secondary" size="md" text="Share Brain" startIcon={<ShareIcon size='lg' />}  />
-            <Button variant="primary" size="lg" text="Add Content" startIcon={<PlusIcon size='lg' />} onclick={()=> setModelOpen(true)}/>
+            <Button variant="secondary" size="md" text="Share Brain" startIcon={<ShareIcon size='lg' />} onclick={shareContent}/>
+            <Button variant="primary" size="lg" text="Add Content" startIcon={<PlusIcon size='lg' />} onclick={() => setModelOpen(true)} />
           </div>
         </div>
 
         <div className='w-full flex flex-wrap gap-x-2 pt-12'>
-         { getAllcontent.map(({title, type, link})=> (
-          <Card  type={type} title={title} link={link} />
-         ))  
-         
-         }
+          {getAllcontent.map(({ title, type, link }) => (
+            <Card type={type} title={title} link={link} />
+          ))
+
+          }
           {/* <Card/> */}
         </div>
       </div>
